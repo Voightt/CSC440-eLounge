91306a784a898b3b66f06e38485517a3301157a7
James Phillips <35739593+jjpfilms@users.noreply.github.com>
2020-11-30 22:49:41 -0500

    Delete elounge-js.js

--- elounge-js.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    function getValue()
-    {
-        var d = document.getElementById("Major-select");
-        var displaytext = d.options[d.selectedIndex].text;
-        document.getElementById("major").value=displaytext;
-    }
-}
