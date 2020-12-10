b38d5efcb960b3ac62443756299f7bbc202ea1a6
Entity511 <70960015+Entity511@users.noreply.github.com>
2020-12-10 13:16:30 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 $db_eLounge = "Students";
 $db_name = "eLounge";
 
-$db = new PDO("mysql:host=localhost;dbname=" . $db_name . ';charset=utf8', $dbuser, $db_pass); 
+$db = new PDO("mysql:host=localhost;dbname=" . $db_name . ';charset=utf8', $db_eLounge); 
 $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
